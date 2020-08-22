67db134048ddb09ca27d04a2ab0411640354a44f
Sumit saha <smtsh9@gmail.com>
2020-08-22 15:12:35 +0530

    Updates

---
+++ precache-manifest.2a22371978c335708e3a3b23c464b82c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e896753382a1fb65c831e6ddb7e3b795",
+    "url": "/index.html"
+  },
+  {
+    "revision": "befbcdaeb695cc72247d",
+    "url": "/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "f2477949b597b6440bbf",
+    "url": "/static/js/2.a636507f.chunk.js"
+  },
+  {
+    "revision": "befbcdaeb695cc72247d",
+    "url": "/static/js/main.677ad329.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
